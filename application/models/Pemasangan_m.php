89ee1d9624735f1e0c1b880f84dc66c3e3cc8956
drkidal <drkidal@bitbucket.org>
2020-06-17 00:14:32 +0800

    perbaikan laporan kenaikan penurunan

--- application/models/Pemasangan_m.php
+++ application/models/Pemasangan_m.php
@@ -1,122 +1,124 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Pemasangan_m extends CI_Model {
-    public function get($id= null)
+class Pemasangan_m extends CI_Model
+{
+    public function get($id = null)
     {
         $this->db->from('pemasangan');
-        if($id != null){
+        if ($id != null) {
             $this->db->where('pemasangan_id', $id);
         }
-        $this->db->join('pelanggan' , 'pelanggan.pelanggan_id=pemasangan.pelanggan_id' , 'left');
-        $this->db->join('kelurahan' , 'kelurahan.kelurahan_id=pelanggan.kelurahan_id' , 'left');
-        $this->db->join('teknisi' , 'teknisi.teknisi_id=pemasangan.teknisi_id' , 'left');
-        $this->db->join('harga' , 'harga.jumlah_id=pemasangan.jumlah_id' , 'left');
-        $this->db->order_by('pemasangan.status','asc');
-        $this->db->order_by('pemasangan.tanggal_pemasangan','asc');
+        $this->db->join('pelanggan', 'pelanggan.pelanggan_id=pemasangan.pelanggan_id', 'left');
+        $this->db->join('kelurahan', 'kelurahan.kelurahan_id=pelanggan.kelurahan_id', 'left');
+        $this->db->join('teknisi', 'teknisi.teknisi_id=pemasangan.teknisi_id', 'left');
+        $this->db->join('harga', 'harga.jumlah_id=pemasangan.jumlah_id', 'left');
+        $this->db->order_by('pemasangan.status', 'asc');
+        $this->db->order_by('pemasangan.tanggal_pemasangan', 'asc');
         $query = $this->db->get();
         return $query;
     }
-    public function getpe($id= null)
+    public function getpe($id = null)
     {
         $this->db->from('pemasangan');
-        if($id != null){
+        if ($id != null) {
             $this->db->where('pemasangan_id', $id);
         }
-        $this->db->join('pelanggan' , 'pelanggan.pelanggan_id=pemasangan.pelanggan_id' , 'left');
-        $this->db->join('kelurahan' , 'kelurahan.kelurahan_id=pelanggan.kelurahan_id' , 'left');
+        $this->db->join('pelanggan', 'pelanggan.pelanggan_id=pemasangan.pelanggan_id', 'left');
+        $this->db->join('kelurahan', 'kelurahan.kelurahan_id=pelanggan.kelurahan_id', 'left');
         $query = $this->db->get();
         return $query;
     }
-    public function getpending($id= null)
+    public function getpending($id = null)
     {
         $this->db->from('pemasangan');
-        if($id != null){
+        if ($id != null) {
             $this->db->where('pemasangan_id', $id);
         }
-        $this->db->join('pelanggan' , 'pelanggan.pelanggan_id=pemasangan.pelanggan_id' , 'left');
-        $this->db->join('kelurahan' , 'kelurahan.kelurahan_id=pelanggan.kelurahan_id' , 'left');
-        $this->db->join('teknisi' , 'teknisi.teknisi_id=pemasangan.teknisi_id' , 'left');
-        $this->db->join('harga' , 'harga.jumlah_id=pemasangan.jumlah_id' , 'left');
+        $this->db->join('pelanggan', 'pelanggan.pelanggan_id=pemasangan.pelanggan_id', 'left');
+        $this->db->join('kelurahan', 'kelurahan.kelurahan_id=pelanggan.kelurahan_id', 'left');
+        $this->db->join('teknisi', 'teknisi.teknisi_id=pemasangan.teknisi_id', 'left');
+        $this->db->join('harga', 'harga.jumlah_id=pemasangan.jumlah_id', 'left');
         $this->db->where('status', 1);
         $query = $this->db->get();
         return $query;
     }
-    public function getterpasang($id= null)
+    public function getterpasang($id = null)
     {
         $this->db->from('pemasangan');
-        if($id != null){
+        if ($id != null) {
             $this->db->where('pemasangan_id', $id);
         }
-        $this->db->join('pelanggan' , 'pelanggan.pelanggan_id=pemasangan.pelanggan_id' , 'left');
-        $this->db->join('kelurahan' , 'kelurahan.kelurahan_id=pelanggan.kelurahan_id' , 'left');
-        $this->db->join('teknisi' , 'teknisi.teknisi_id=pemasangan.teknisi_id' , 'left');
-        $this->db->join('harga' , 'harga.jumlah_id=pemasangan.jumlah_id' , 'left');
-        $this->db->where('status',2);
-        
-        $this->db->order_by('pemasangan.status','asc');
+        $this->db->join('pelanggan', 'pelanggan.pelanggan_id=pemasangan.pelanggan_id', 'left');
+        $this->db->join('kelurahan', 'kelurahan.kelurahan_id=pelanggan.kelurahan_id', 'left');
+        $this->db->join('teknisi', 'teknisi.teknisi_id=pemasangan.teknisi_id', 'left');
+        $this->db->join('harga', 'harga.jumlah_id=pemasangan.jumlah_id', 'left');
+        $this->db->where('status', 2);
+
+        $this->db->order_by('pemasangan.status', 'asc');
         $query = $this->db->get();
-         // echo $this->db->last_query();die();
+        // echo $this->db->last_query();die();
         return $query;
     }
-    public function getjangkauan($id= null)
+    public function getjangkauan($id = null)
     {
         $this->db->from('pemasangan');
-        if($id != null){
+        if ($id != null) {
             $this->db->where('pemasangan_id', $id);
         }
-        $this->db->join('pelanggan' , 'pelanggan.pelanggan_id=pemasangan.pelanggan_id' , 'left');
-        $this->db->join('kelurahan' , 'kelurahan.kelurahan_id=pelanggan.kelurahan_id' , 'left');
-        $this->db->join('teknisi' , 'teknisi.teknisi_id=pemasangan.teknisi_id' , 'left');
-        $this->db->join('harga' , 'harga.jumlah_id=pemasangan.jumlah_id' , 'left');
+        $this->db->join('pelanggan', 'pelanggan.pelanggan_id=pemasangan.pelanggan_id', 'left');
+        $this->db->join('kelurahan', 'kelurahan.kelurahan_id=pelanggan.kelurahan_id', 'left');
+        $this->db->join('teknisi', 'teknisi.teknisi_id=pemasangan.teknisi_id', 'left');
+        $this->db->join('harga', 'harga.jumlah_id=pemasangan.jumlah_id', 'left');
         $this->db->where('status', 3);
-    
+
         $query = $this->db->get();
-         // echo $this->db->last_query();die();
+        // echo $this->db->last_query();die();
         return $query;
     }
-    public function getnonaktif($id= null)
+    public function getnonaktif($id = null)
     {
         $this->db->from('pemasangan');
-        if($id != null){
+        if ($id != null) {
             $this->db->where('pemasangan_id', $id);
         }
-        $this->db->join('pelanggan' , 'pelanggan.pelanggan_id=pemasangan.pelanggan_id' , 'left');
-        $this->db->join('kelurahan' , 'kelurahan.kelurahan_id=pelanggan.kelurahan_id' , 'left');
-        $this->db->join('teknisi' , 'teknisi.teknisi_id=pemasangan.teknisi_id' , 'left');
-        $this->db->join('harga' , 'harga.jumlah_id=pemasangan.jumlah_id' , 'left');
+        $this->db->join('pelanggan', 'pelanggan.pelanggan_id=pemasangan.pelanggan_id', 'left');
+        $this->db->join('kelurahan', 'kelurahan.kelurahan_id=pelanggan.kelurahan_id', 'left');
+        $this->db->join('teknisi', 'teknisi.teknisi_id=pemasangan.teknisi_id', 'left');
+        $this->db->join('harga', 'harga.jumlah_id=pemasangan.jumlah_id', 'left');
         $this->db->where('status', 4);
-    
+
         $query = $this->db->get();
-         // echo $this->db->last_query();die();
+        // echo $this->db->last_query();die();
         return $query;
     }
 
-    public function getaktif($id= null)
+    public function getaktif($id = null)
     {
         $this->db->from('pemasangan');
-        if($id != null){
+        if ($id != null) {
             $this->db->where('pemasangan_id', $id);
         }
         $this->db->where('status', 2);
         $query = $this->db->get();
         return $query;
     }
-    
+
     public function ambil_data($tabel)
     {
         $this->db->from($tabel);
-        
+
         $query = $this->db->get();
         return $query->result_array();
     }
-    public function rubah_status($id){
+    public function rubah_status($id)
+    {
         $data = $this->get($id);
         $data = $data->result_array();
-        if ($data[0]['status'] == 'Belum'){
+        if ($data[0]['status'] == 'Belum') {
             $params['status'] = 'Sudah';
         }
-        if ($data[0]['status'] == 'Sudah'){
+        if ($data[0]['status'] == 'Sudah') {
             $params['status'] = 'Belum';
         }
         $this->db->where('pemasangan_id', $id);
@@ -131,7 +133,6 @@ class Pemasangan_m extends CI_Model {
         $params['teknisi_id'] = $post['teknisi_id'];
         $params['status'] = $post['status'];
         $this->db->insert('pemasangan', $params);
-       
     }
     public function edit($post)
     {
@@ -144,18 +145,18 @@ class Pemasangan_m extends CI_Model {
         $this->db->where('pemasangan_id', $post['pemasangan_id']);
         $this->db->update('pemasangan', $params);
         if ($params['status'] == 2)
-        $this->pembayaran_add($params['pelanggan_id']);
+            $this->pembayaran_add($params['pelanggan_id']);
     }
-    function pembayaran_add($pelanggan_id){
-        
+    function pembayaran_add($pelanggan_id)
+    {
+
         $params['pelanggan_id'] = $pelanggan_id;
         $this->db->insert('pembayaran', $params);
-
     }
     public function del($id)
-	{
-		$this->db->where('pemasangan_id', $id);
-		$this->db->delete('pemasangan');
+    {
+        $this->db->where('pemasangan_id', $id);
+        $this->db->delete('pemasangan');
     }
     public function getpenambahan($pemasangan)
     {
@@ -164,72 +165,41 @@ class Pemasangan_m extends CI_Model {
         $this->db->select('count(pemasangan_id) as num');
         $this->db->select('monthname(tanggal_pemasangan) as bulan');
         $this->db->from('pemasangan');
-        $this->db->where('status','2');
-        $this->db->where('year(tanggal_pemasangan)',$tahun);
+        $this->db->where('status', '2');
+        $this->db->where('year(tanggal_pemasangan)', $tahun);
         $this->db->group_by('month(tanggal_pemasangan)');
         $q = $this->db->get_where();
         $q = $q->result_array();
         //echo $this->db->last_query();die();
         return $q;
-        
     }
 
-    public function getplus($pemasangan)
+    public function getplusminus($tahun)
     {
-        $tahun = $this->input->post('tahun');
-        $kondisi = "";
-        $this->db->select('count(pemasangan_id) as num');
-        $this->db->select('monthname(tanggal_pemasangan) as bulan');
-        $this->db->from('pemasangan');
-        $this->db->where('status','2');
-        $this->db->where('year(tanggal_pemasangan)',$tahun);
-        $this->db->group_by('month(tanggal_pemasangan)');
-        $q = $this->db->get_where();
-        $q = $q->result_array();
-        //echo $this->db->last_query();die();
-        return $q;
-        
+        $query = $this->db->query('SELECT A.bulan, COALESCE(A.plus,0) as plus, COALESCE(B.minus,0) as minus FROM (SELECT monthname(tanggal_pemasangan) as bulan, count(pemasangan_id) as plus FROM pemasangan WHERE status = "2" AND year(tanggal_pemasangan) = ' . $tahun . ' GROUP BY month(tanggal_pemasangan)) AS A LEFT JOIN (SELECT monthname(tanggal_pemasangan) as bulan, count(pemasangan_id) as minus FROM pemasangan LEFT JOIN pelanggan ON pelanggan.pelanggan_id=pemasangan.pelanggan_id LEFT JOIN pemutusan ON pemutusan.pelanggan_id=pemasangan.pelanggan_id WHERE status = "4" AND year(tanggal_pemutusan) = ' . $tahun . ' GROUP BY month(tanggal_pemutusan)) AS B ON A.bulan = B.bulan');
+        return $query->result_array();
     }
 
-    public function getminus($pengurangan)
-    {
-        $tahun = $this->input->post('tahun');
-        $kondisi = "";
-        $this->db->select('count(pemasangan_id) as sum');
-        $this->db->select('monthname(tanggal_pemutusan) as month');
-        $this->db->from('pemasangan');
-        $this->db->join('pelanggan','pelanggan.pelanggan_id=pemasangan.pelanggan_id','left');
-        $this->db->join('pemutusan','pemutusan.pelanggan_id=pemasangan.pelanggan_id','left');
-        $this->db->where('status','4');
-        $this->db->where('year(tanggal_pemutusan)',$tahun);
-        $this->db->group_by('month(tanggal_pemutusan)');
-        $q = $this->db->get_where();
-        $q = $q->result_array();
-        //echo $this->db->last_query();die();
-        return $q;
-        
-    }
     public function getByDate($tgl_awal, $tgl_akhir)
     {
         $kondisi = "";
         $this->db->select('');
         $this->db->from('pemasangan');
-        $this->db->join('pelanggan','pelanggan.pelanggan_id=pemasangan.pelanggan_id','left');
-        $this->db->order_by('pemasangan.status','asc');
-        $this->db->order_by('pemasangan.tanggal_pemasangan','asc');
-        $this->db->where('status',2);
-        if ($tgl_awal != "" && $tgl_akhir==""){
-            $this->db->where('tanggal_pemasangan >=',$tgl_awal);
-        } else if ($tgl_awal == "" && $tgl_akhir!=""){
-            $this->db->where('tanggal_pemasangan <=',$tgl_akhir);
-        } else  if ($tgl_awal != "" && $tgl_akhir!=""){
-            $this->db->where('tanggal_pemasangan >=',$tgl_awal);
-            $this->db->where('tanggal_pemasangan <=',$tgl_akhir);
+        $this->db->join('pelanggan', 'pelanggan.pelanggan_id=pemasangan.pelanggan_id', 'left');
+        $this->db->order_by('pemasangan.status', 'asc');
+        $this->db->order_by('pemasangan.tanggal_pemasangan', 'asc');
+        $this->db->where('status', 2);
+        if ($tgl_awal != "" && $tgl_akhir == "") {
+            $this->db->where('tanggal_pemasangan >=', $tgl_awal);
+        } else if ($tgl_awal == "" && $tgl_akhir != "") {
+            $this->db->where('tanggal_pemasangan <=', $tgl_akhir);
+        } else  if ($tgl_awal != "" && $tgl_akhir != "") {
+            $this->db->where('tanggal_pemasangan >=', $tgl_awal);
+            $this->db->where('tanggal_pemasangan <=', $tgl_akhir);
         }
         $q = $this->db->get_where();
         $q = $q->result_array();
         return $q;
-        
     }
     public function getByDateUp($tgl_awal, $tgl_akhir)
     {
@@ -238,25 +208,22 @@ class Pemasangan_m extends CI_Model {
         $this->db->select('count(status) as penambahan');
         $this->db->select('count(pemutusan_id) as pengurangan');
         $this->db->from('pelanggan');
-        $this->db->join('pemasangan','pemasangan.pelanggan_id=pelanggan.pelanggan_id','left');
-        $this->db->join('pemutusan','pemutusan.pelanggan_id=pelanggan.pelanggan_id','left');
-        $this->db->where('status',2);
-        if ($tgl_awal != "" && $tgl_akhir==""){
-            $this->db->where('tanggal_pemasangan >=',$tgl_awal);
-        } else if ($tgl_awal == "" && $tgl_akhir!=""){
-            $this->db->where('tanggal_pemasangan <=',$tgl_akhir);
-        } else  if ($tgl_awal != "" && $tgl_akhir!=""){
-            $this->db->where('tanggal_pemasangan >=',$tgl_awal);
-            $this->db->where('tanggal_pemasangan <=',$tgl_akhir);
+        $this->db->join('pemasangan', 'pemasangan.pelanggan_id=pelanggan.pelanggan_id', 'left');
+        $this->db->join('pemutusan', 'pemutusan.pelanggan_id=pelanggan.pelanggan_id', 'left');
+        $this->db->where('status', 2);
+        if ($tgl_awal != "" && $tgl_akhir == "") {
+            $this->db->where('tanggal_pemasangan >=', $tgl_awal);
+        } else if ($tgl_awal == "" && $tgl_akhir != "") {
+            $this->db->where('tanggal_pemasangan <=', $tgl_akhir);
+        } else  if ($tgl_awal != "" && $tgl_akhir != "") {
+            $this->db->where('tanggal_pemasangan >=', $tgl_awal);
+            $this->db->where('tanggal_pemasangan <=', $tgl_akhir);
         }
         $q = $this->db->get_where();
         $q = $q->result_array();
         //echo $this->db->last_query();die();
         //print_r($q);die();
         return $q;
-        
-        
-        
     }
     public function getByDateDown($tgl_awal, $tgl_akhir)
     {
@@ -264,16 +231,16 @@ class Pemasangan_m extends CI_Model {
         $this->db->select('*');
         $this->db->select('count(status) as pengurangan');
         $this->db->from('pelanggan');
-        $this->db->join('pemasangan','pemasangan.pelanggan_id=pelanggan.pelanggan_id','left');
-        $this->db->join('pemutusan','pemutusan.pelanggan_id=pelanggan.pelanggan_id','left');
-        $this->db->where('status',4);
-        if ($tgl_awal != "" && $tgl_akhir==""){
-            $this->db->where('tanggal_pemutusan >=',$tgl_awal);
-        } else if ($tgl_awal == "" && $tgl_akhir!=""){
-            $this->db->where('tanggal_pemutusan <=',$tgl_akhir);
-        } else  if ($tgl_awal != "" && $tgl_akhir!=""){
-            $this->db->where('tanggal_pemutusan >=',$tgl_awal);
-            $this->db->where('tanggal_pemutusan <=',$tgl_akhir);
+        $this->db->join('pemasangan', 'pemasangan.pelanggan_id=pelanggan.pelanggan_id', 'left');
+        $this->db->join('pemutusan', 'pemutusan.pelanggan_id=pelanggan.pelanggan_id', 'left');
+        $this->db->where('status', 4);
+        if ($tgl_awal != "" && $tgl_akhir == "") {
+            $this->db->where('tanggal_pemutusan >=', $tgl_awal);
+        } else if ($tgl_awal == "" && $tgl_akhir != "") {
+            $this->db->where('tanggal_pemutusan <=', $tgl_akhir);
+        } else  if ($tgl_awal != "" && $tgl_akhir != "") {
+            $this->db->where('tanggal_pemutusan >=', $tgl_awal);
+            $this->db->where('tanggal_pemutusan <=', $tgl_akhir);
         }
         $q = $this->db->get_where();
         $q = $q->result_array();
@@ -281,5 +248,4 @@ class Pemasangan_m extends CI_Model {
         //print_r($q);die();
         return $q;
     }
-
 }
--- application/views/laporan/plusminus_detail.php
+++ application/views/laporan/plusminus_detail.php
@@ -1,61 +1,77 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-	<title><?php echo strtoupper($title); ?></title>
-	<base href="<?php echo base_url() ?>">
+    <title><?php echo strtoupper($title); ?></title>
+    <base href="<?php echo base_url() ?>">
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     <!-- bootstrap 3.0.2 -->
-    <link href="<?=base_url()?>assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?= base_url() ?>assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        .tandatangan {
+            text-align: center;
+            margin-left: 600px;
+            line-height: 5em;
+        }
+    </style>
 </head>
-<body onload="print()">
-	<center>
-		<table>
-			<tr>
-				<td>
-					<img src="<?php echo base_url('assets/') ?>/img/logo-mu.png" style="width: 100px; height: 100px;">
-				</td>
-				<td>
-					<center>
-						<h3>PT. Cahaya MU Vision</h3>
-					<h5>Jl.Sungai Andai No.07 RT.27 Telepon/Fax(0511)3301345 , (0511)6263302 Banjarmasin, 70122</h5>
-					</center>
-				</td>
-			</tr>
-		</table>
-        <?php $tahun = $this->input->post('tahun');?>
-		<h4><?php echo strtoupper($title); ?> <?php echo $tahun?></h4>
-	</center>
-    <table class="table table-bordered">
-                <thead>
-                    <tr>
-                        <th>No</th>
-                        <th>Bulan</th>
-                        <th>Penambahan</th>
-                        <th>Bulan</th>
-                        <th>Pengurangan</th>
-                        <th>Jumlah</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php $no = 1;
-                    foreach($p as $x) { ?>
-                    <tr>
-                        <td><?php echo $no++?>.</td>
-                        <td><?php echo $x['bulan']?></td>
-                        <td><?php echo $x['num']?></td>
-                        <?php foreach($z as $p) { ?>
-                        <td><?php echo $p['month']?></td> 
-                        <td><?php echo $p['sum']?></td> 
-                        <td><?php echo $x['num'] - $p['sum'] ?></td>
-                    </tr>
-                    <?php }?>
-                        
 
-                      
-                    </tr>
-                    <?php
-                    } ?>
-                </tbody>
+<body onload="print()">
+    <center>
+        <table>
+            <tr>
+                <td>
+                    <img src="<?php echo base_url('assets/') ?>/img/logo-mu.png" style="width: 100px; height: 100px;">
+                </td>
+                <td>
+                    <center>
+                        <h3>PT. Cahaya MU Vision</h3>
+                        <h5>Jl.Sungai Andai No.07 RT.27 Telepon/Fax(0511)3301345 , (0511)6263302 Banjarmasin, 70122</h5>
+                    </center>
+                </td>
+            </tr>
+        </table>
+        <br>
+        <?php $tahun = $this->input->post('tahun'); ?>
+        <h4><?php echo strtoupper($title); ?> <?php echo $tahun ?></h4>
+    </center>
+    <table style="width: 70%; margin-left:auto; margin-right:auto;" class="table table-bordered">
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>Bulan</th>
+                <th>Penambahan</th>
+                <th>Pengurangan</th>
+                <th>(+/-)</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php $no = 1;
+            foreach ($plusminus as $pm) { ?>
+                <tr>
+                    <td><?php echo $no++ ?>.</td>
+                    <td><?php echo $pm['bulan'] ?></td>
+                    <td><?php echo $pm['plus'] ?></td>
+                    <td><?php echo $pm['minus'] ?></td>
+                    <td><?php
+                        $hasil = $pm['plus'] + $pm['minus'];
+
+                        if ($hasil > 0) {
+                            echo '<font color="blue">' . $hasil . '</font>';
+                        } else if ($hasil < 0) {
+                            echo '<font color="red">' . $hasil . '</font>';
+                        } else {
+                            echo $hasil;
+                        }
+                        ?>
+                    </td>
+                </tr>
+            <?php } ?>
+        </tbody>
     </table>
-    <div class="float-md-right"><strong><p> Pimpinan &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</p></strong></div></br></br></br></br></br></br>
-    <div class="float-md-right"><p><?php echo $this->fungsi->pimpinan()->name ?> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</p></div>
+    <div class="tandatangan">
+        Pimpinan
+    </div>
+    <div class="tandatangan">
+        <?php echo $this->fungsi->pimpinan()->name ?>
+    </div>
